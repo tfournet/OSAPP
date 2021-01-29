335014eedacc3c607320b581a5e5f5b1d30a5cc2
Tim Fournet <tfournet@radersolutions.com>
2021-01-29 14:55:21 -0600

    oneline now

--- host_setup/cockpit/osapp_deploy.js
+++ host_setup/cockpit/osapp_deploy.js
@@ -13,15 +13,7 @@ const result = document.getElementById("result");
 const button = document.getElementById("config");
 
 function setup_run() {
-    cockpit.spawn(["/usr/local/osapp/begin_from_cockpit.sh", custAbbr.value, custTld.value, siteSubnet.value, 
-        extDns1.value,
-        extDns2.value,
-        cybercns_hostname.value,
-        cybercns_siteId.value,
-        Perch_URL.value,
-        OPNSense_URL.value,
-        OPNSense_SHA256.value
-    ])
+    cockpit.spawn(["/usr/local/osapp/begin_from_cockpit.sh", custAbbr.value, custTld.value, siteSubnet.value, extDns1.value, extDns2.value, cybercns_hostname.value, cybercns_siteId.value, Perch_URL.value, OPNSense_URL.value, OPNSense_SHA256.value])
         .stream(setup_output)
         .then(setup_success)
         .catch(setup_fail);
