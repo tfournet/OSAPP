7ae1612209a80bb111ae4b8eea3b7de530ac260c
Tim Fournet <tfournet@radersolutions.com>
2021-01-29 14:05:02 -0600

    testing

--- host_setup/cockpit/osapp_deploy.js
+++ host_setup/cockpit/osapp_deploy.js
@@ -1,4 +1,4 @@
-const address = document.getElementById("extDns1");
+const address = document.getElementById("cybercns_hostname");
 const output = document.getElementById("output");
 const result = document.getElementById("result");
 const button = document.getElementById("config");
