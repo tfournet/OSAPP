7246b63f78d31b3172e8b397794f0dc511abbb23
Tim Fournet <tfournet@radersolutions.com>
2021-01-29 11:49:41 -0600

    net

--- remove_everything.sh
+++ remove_everything.sh
@@ -7,6 +7,7 @@ rm -rf /usr/local/containers/cybercns*
 /usr/local/osapp/host_setup/reset_virsh.sh 
 
 /usr/local/osapp/host_setup/reset_networking.sh 
+/usr/local/osapp/host_setup/host_networking.sh 
 
 rm -rf /etc/osapp 
 
